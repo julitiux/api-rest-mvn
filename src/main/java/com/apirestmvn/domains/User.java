9480e88c424c653ade7956167bc267f8471ecfaf
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-14 17:22:49 -0600

    add domain User

---
+++ src/main/java/com/apirestmvn/domains/User.java
@@ -0,0 +1,35 @@
+package com.apirestmvn.domains;
+
+public class User {
+
+  String name;
+  String firstName;
+  String lastName;
+  String mail;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getFirstName() {
+    return firstName;
+  }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+  public String getLastName() {
+    return lastName;
+  }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+  public String getMail() {
+    return mail;
+  }
+  public void setMail(String mail) {
+    this.mail = mail;
+  }
+}
