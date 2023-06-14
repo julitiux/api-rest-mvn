e66ceca2aed882fb832e2d482788101fe7469d6b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-14 12:23:15 -0600

    first commit api-rest-mvn

---
+++ src/main/java/com/apirestmvn/ApiRestMvnApplication.java
@@ -0,0 +1,13 @@
+package com.apirestmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestmvn/ApiRestMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
