de78dbbd6aa381567d35bef237a5fd9d8eef86c1
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-17 23:32:40 -0600

    mapping by path + method

--- src/main/java/com/apirestmvn/controllers/RequestMapController.java
+++ src/main/java/com/apirestmvn/controllers/RequestMapController.java
@@ -2,6 +2,7 @@ package com.apirestmvn.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,6 +11,11 @@ public class RequestMapController {
   @RequestMapping("/simpleMapping/path")
   public @ResponseBody String path() {
     return "mapping by path";
+  }
+
+  @RequestMapping(value = "/simpleMapping/method", method = RequestMethod.GET)
+  public @ResponseBody String byMethod() {
+    return "mapping by path + method";
   }
 
 }
