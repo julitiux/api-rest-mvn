747132c3c74263c7bcf5a56aa3f92331203ff57b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-17 23:30:40 -0600

    mapping by path

--- src/main/java/com/apirestmvn/controllers/RequestMapController.java
+++ src/main/java/com/apirestmvn/controllers/RequestMapController.java
@@ -1,7 +1,15 @@
 package com.apirestmvn.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RequestMapController {
+
+  @RequestMapping("/simpleMapping/path")
+  public @ResponseBody String path() {
+    return "mapping by path";
+  }
+
 }
