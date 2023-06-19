7b44d63b3438c7533f6c774f89eeaa450975538d
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-18 19:13:08 -0600

    mapping by path + method + header negation

--- src/main/java/com/apirestmvn/controllers/RequestMapController.java
+++ src/main/java/com/apirestmvn/controllers/RequestMapController.java
@@ -8,29 +8,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RequestMapController {
 
-    @RequestMapping("/simpleMapping/path")
-    public @ResponseBody String path() {
-        return "mapping by path";
-    }
+  @RequestMapping("/simpleMapping/path")
+  public @ResponseBody String path() {
+    return "mapping by path";
+  }
 
-    @RequestMapping(value = "/simpleMapping/method", method = RequestMethod.GET)
-    public @ResponseBody String byMethod() {
-        return "mapping by path + method";
-    }
+  @RequestMapping(value = "/simpleMapping/method", method = RequestMethod.GET)
+  public @ResponseBody String byMethod() {
+    return "mapping by path + method";
+  }
 
-    @RequestMapping(value = "/simpleMapping/parameter", method = RequestMethod.GET, params = "foo")
-    public @ResponseBody String byParameter() {
-        return "mapping by path + method + a search parameter";
-    }
+  @RequestMapping(value = "/simpleMapping/parameter", method = RequestMethod.GET, params = "foo")
+  public @ResponseBody String byParameter() {
+    return "mapping by path + method + a search parameter";
+  }
 
-    @RequestMapping(value = "/simpleMapping/parameter", method = RequestMethod.GET, params = "!foo")
-    public @ResponseBody String byParameterNegation() {
-        return "mapping by path + method + a search parameter negation";
-    }
+  @RequestMapping(value = "/simpleMapping/parameter", method = RequestMethod.GET, params = "!foo")
+  public @ResponseBody String byParameterNegation() {
+    return "mapping by path + method + a search parameter negation";
+  }
 
-    @RequestMapping(value = "/simpleMapping/header", method = RequestMethod.GET, headers = "Accept=text/plain")
-    public @ResponseBody String byHeader() {
-        return "mapping by path + method + a header";
-    }
+  @RequestMapping(value = "/simpleMapping/header", method = RequestMethod.GET, headers = "Accept=text/plain")
+  public @ResponseBody String byHeader() {
+    return "mapping by path + method + a header";
+  }
+
+  @RequestMapping(value = "/simpleMapping/notHeader", method = RequestMethod.GET, headers = "!FooHeader")
+  public @ResponseBody String byHeaderNegation() {
+    return "mapping by path + method + header negation";
+  }
 
 }
